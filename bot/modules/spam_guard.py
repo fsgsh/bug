88276e32cb116d68f96c99b8dba7529be6be6d5f
Nitan Marcel Alexandru <nitan.marcel@protonmail.com>
2020-08-09 20:59:15 +0300

    Restrict external urls in on-topic groups

--- bot/modules/spam_guard.py
+++ bot/modules/spam_guard.py
@@ -17,53 +17,66 @@ URL_BLACKLIST = [re.sub(r"^http(\w?)://", "", line.rstrip('\n').lower())
              for line in open("urls.blacklist", "r").readlines()]
 
 
+TELEGRAM_DOMAINS = ['t.me',
+                    'telegram.org',
+                    'telegram.dog',
+                    'telegra.ph',
+                    'tdesktop.com',
+                    'telesco.pe',
+                    'graph.org',
+                    'contest.dev']
+
+def _get_fld(uri):
+    if not re.search(r"^http(\w?)://", uri):
+        uri = re.sub("^", "http://", uri)
+    try:
+        return get_fld(uri)
+    except TldDomainNotFound:
+        return None
 
 def check_url(url):
     if re.sub(r"^http(\w?)://", "", url.lower()) in URL_BLACKLIST:
         return True
-    if not re.search(r"^http(\w?)://", url):
-        url = re.sub("^", "http://", url)
-        try:
-            if get_fld(url) in DOMAIN_BLACKLIST:
-                return True
-        except TldDomainNotFound:
-            return True
+    fdl = _get_fld(url)
+    if not fdl:
+        return True
+    elif fdl in DOMAIN_BLACKLIST:
+        return True
 
-@bot.on(NewMessage(func=lambda x: not x.is_private))
+@bot.on(NewMessage(func=lambda e: not e.is_private))
 async def spam_guard(event):
     """Checks if the user sends any type of link, and compares it against the blacklisted domains.
         If the domain is blacklisted the user will be muted for 5 minutes
     """
+    FORMAT = "**Banned: [{first_name}](tg://user?id={user_id})\n\n**Reason:** __Spam!!__"
     if event.entities:
-        is_spam = False
-        urls = []
         for ent in event.entities:
+            url = str()
             if isinstance(ent, MessageEntityTextUrl):
-                urls.append(ent.url)
-
+                url = ent.url
             if isinstance(ent, MessageEntityUrl):
-                urls.append(event.text[ent.offset:].split()[0])
-
-        for url in urls:
-            is_spam = check_url(url)
-            if is_spam:
-                chat = await event.get_chat()
-                user = await event.get_sender()
+                url = event.text[ent.offset:].split()[0]
+            print(url, bool(url))
+            if not bool(url):
+                return
+            chat = await event.get_chat()
+            user = await event.get_sender()
+            check = check_url(url)
+            fld = _get_fld(url)
+            if check or fld not in TELEGRAM_DOMAINS:
                 try:
                     result = await bot(GetParticipantRequest(chat, user))
+                    if not isinstance(result.participant, (ChannelParticipantCreator, ChannelParticipantCreator)):
+                        if check:
+                            await event.delete()
+                            await bot.edit_permissions(chat, user, view_messages=False)
+                            await bot.send_message(chat, FORMAT.format(first_name=user.first_name, user_id=user.id), link_preview=False)
+                        elif chat.id in [1374518507, 1195021050, 1361570927]:
+                            await event.delete()
+                            await event.delete()
+                            await bot.send_message(chat,
+                                                     "External links are not allowed in this chat! Please move to [OffTopic Chat](https://t.me/AuroraOT)")
+
                 except UserNotParticipantError:
                     await bot.edit_permissions(chat, user, view_messages=False)
-                    await bot.send_message(chat,
-                                           f"**Banned:** [{user.first_name}](tg://user?id={user.id})\n\n**Reason:** __Spam!!__")
-                    return
-                if isinstance(result.participant, (ChannelParticipantCreator, ChannelParticipantAdmin)):
-                    return
-                await event.delete()
-                try:
-                    await bot.edit_permissions(chat, user, send_messages=False,
-                                               until_date=datetime.timedelta(minutes=5))
-                    await bot.send_message(chat,
-                                           f"**Restricted **[{user.first_name}](tg://user?id={user.id}) **for `5` minutes**\n\n**Reason:** __Use of restricted url!!__")
-                except UserAdminInvalidError:
-                    pass
-                continue
+                    await bot.send_message(chat, FORMAT.format(first_name=user.first_name, user_id=user.id))
